6e9cdc4bdf1e0cad912c3e8449f326cad82fa2c6
yilun.lee <yilun.lee@ailabs.tw>
2023-05-20 17:06:18 +0800

    add docs

---
+++ docs/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["argtool"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"argtool/argitem/enum.ArgValue.html\" title=\"enum argtool::argitem::ArgValue\">ArgValue</a>","synthetic":false,"types":["argtool::argitem::ArgValue"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- scripts/pre_push.sh
+++ scripts/pre_push.sh
@@ -1,14 +1,15 @@
 
 # ADD these to pre-push.sample
 # get base
-BASEDIR="$(git rev-parse --show-toplevel)"
-cd "$BASEDIR"
+# BASEDIR="$(git rev-parse --show-toplevel)"
+# cd "$BASEDIR"
 
 # add doc
+rm -rf target/doc || true
 cargo doc --no-deps --workspace  
-rm -rf docs || true
-cp -r target/docs .
+cp -r target/doc docs
 git add docs 
+git add -u
 git commit -m "add docs" 
 
 
