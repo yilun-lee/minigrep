a5957f69ce2736fa1eec997f840546bcdcad1158
yilun.lee <yilun.lee@ailabs.tw>
2023-05-20 13:42:57 +0800

    impl ArgValue get_bool to be shown in doc

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,3 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["argtool"] = {"name":"","files":["argitem.rs","argroup.rs","lib.rs"]};
-sourcesIndex["grep"] = {"name":"","files":["main.rs"]};
 createSourceSidebar();
