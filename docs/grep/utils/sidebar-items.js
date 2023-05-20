57b1deedaebea1dfa3347b1973ace79ce88f4044
yilun.lee <yilun.lee@ailabs.tw>
2023-05-20 21:04:18 +0800

    add docs

---
+++ docs/grep/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["read_file","read file and retuen buffer reader"]],"struct":[["FileReader",""]]});
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["argtool"] = {"name":"","files":["argitem.rs","argroup.rs","lib.rs"]};
-sourcesIndex["grep"] = {"name":"","files":["main.rs"]};
+sourcesIndex["grep"] = {"name":"","files":["main.rs","test.rs","utils.rs"]};
 createSourceSidebar();
