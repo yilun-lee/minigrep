ba40c56e4413476f482f344e787498284e78c001
yilun.lee <yilun.lee@ailabs.tw>
2023-05-27 20:07:14 +0800

    add docs

--- docs/grep/utils/sidebar-items.js
+++ docs/grep/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["MyErrors"],"fn":["read_file"],"struct":["FileReader"]};
+window.SIDEBAR_ITEMS = {"enum":["MyErrors"],"fn":["read_file"],"struct":["FileReader","LinePrinter","PathGlober"]};
